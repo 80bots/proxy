74149161e52bb8e8b6e27a2bfde3c44b764e67fa
romfis <roman.bikbaiev.dev@gmail.com>
2020-10-08 17:54:00 +0300

    Test pem has been added.

--- mac/run_13.211.252.49.sh
+++ mac/run_13.211.252.49.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 chmod +x ./run.sh
-chmod 400 ../test_data/alexandr.pem
-./run.sh 13.211.252.49 ubuntu ../test_data/alexandr.pem
+chmod 400 ../test_data/test-proxy.pem
+./run.sh 13.59.107.162 ubuntu ../test_data/test-proxy.pem
