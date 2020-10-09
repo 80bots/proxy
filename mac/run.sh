9966369700ac9708580817b0af2f4d67cadb1886
romfis <roman.bikbaiev.dev@gmail.com>
2020-10-09 15:52:39 +0300

    Test pem has been added.

--- mac/run.sh
+++ mac/run.sh
@@ -3,10 +3,10 @@ EC2_NODE=$1
 EC2_USER=$2
 EC2_PEM=$3
 
-chmod +x ./brook
-./brook socks5 --socks5 127.0.0.1:9999 &
-BROOK_PID=$!
-echo "Brook started on: $BROOK_PID"
+#chmod +x ./brook
+#./brook server -l :9999 -p hello &
+#BROOK_PID=$!
+#echo "Brook started on: $BROOK_PID"
 
 echo "Started tunnel at 9999 port"
 ssh -i $EC2_PEM -N -R 9999:127.0.0.1:9999 $EC2_USER@$EC2_NODE
